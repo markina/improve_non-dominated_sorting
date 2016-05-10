f173400bbfc7d649fc9907cc9adad3e62fe99c6d
markina <margaritam2706@gmail.com>
2016-05-10 19:15:25 +0300

    add plots with one rank

--- python/plot_m.py
+++ python/plot_m.py
@@ -3,8 +3,8 @@ from matplotlib import pyplot as plt
 # постоение графика по name N M
 
 N = 100000
-M = 20
-name = "cube_opt"
+M = 4
+name = "cube"
 prefix = "_result.txt"
 
 full_name = name + "_" + str(N) + "_" + str(M) + prefix
@@ -21,7 +21,7 @@ with open(full_name) as f:
             x.append(n)
             y.append((Tb - Tf) / max(Tf, Tb))
 
-plt.title('M = ' + str(M))
+plt.title(name)
 plt.semilogx(x, y, 'bo', label = 'M = ' + str(M))
 plt.semilogx(x, [0] * len(x), 'r-', label = 'y = 0')
 # plt.plot(x, y, 'b.', label='M = 20')
@@ -30,6 +30,7 @@ plt.ylabel('t')
 plt.legend(loc = 4)
 
 plt.xlim(0, 110000)
+# plt.ylim(0, 1.05)
 plt.show()
 
 
